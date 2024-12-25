428ef6c853b7c3869e081dfc81d5ee0be676ed8e
tsantos8080 <tsantos8080@gmail.com>
2024-12-25 16:27:28 -0300

    add some neovim keymaps

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -16,6 +16,13 @@ vim.g.mapleader = " "
 vim.wo.relativenumber = true
 vim.wo.number = true
 
+-- Remap '<' and '>' in visual mode to reselect the last visual selection after indenting
+vim.api.nvim_set_keymap('v', '<', '<gv', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true, silent = true })
+
+-- Switch to the most recent buffer
+vim.api.nvim_set_keymap('n', '<tab>', ':buffer #<CR>', { noremap = true, silent = true })
+
 vim.api.nvim_set_keymap('n', '<leader>r', ':lua require("scripts").run_test()<cr>', {})
 
 require("lazy").setup("plugins")

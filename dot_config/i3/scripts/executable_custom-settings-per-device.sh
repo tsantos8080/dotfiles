81b699e56327b2e2ffe776a4d9469d99b86df919
tsantos8080 <tsantos8080@gmail.com>
2025-04-10 10:25:08 -0300

    fix keyboard layout

--- dot_config/i3/scripts/executable_custom-settings-per-device.sh
+++ dot_config/i3/scripts/executable_custom-settings-per-device.sh
@@ -3,7 +3,7 @@
 if hostnamectl | grep -q "Chassis: desktop"; then
 	setxkbmap -layout us -variant intl
 else
-	setxkbmap -layout us,br -variant intl,thinkpad -option 'grp:ctrl_alt_toggle'
+	setxkbmap -layout us,br -variant intl,thinkpad -model thinkpad -option 'grp:win_space_toggle'
 
 	xrandr --output HDMI-1 --mode 1920x1080 --rate 120
 fi

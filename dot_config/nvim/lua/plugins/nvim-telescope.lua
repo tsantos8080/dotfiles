7d0f006dfe8f846aaab5a579d182715a764778c9
tsantos8080 <tsantos8080@gmail.com>
2025-04-24 11:26:37 -0300

    add telescope keybind to show old files from relative path

--- dot_config/nvim/lua/plugins/nvim-telescope.lua
+++ dot_config/nvim/lua/plugins/nvim-telescope.lua
@@ -18,6 +18,9 @@ return {
 
     -- keymaps
     vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
+    vim.keymap.set("n", "<leader>fo", function()
+      require("telescope.builtin").oldfiles({ cwd_only = true })
+    end, { desc = "Oldfiles (relative)" })
     vim.keymap.set('n', '<leader>fg', '<cmd>Telescope git_status<cr>')
     vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
     vim.keymap.set('n', '<leader>sg', '<cmd>Telescope live_grep<cr>')

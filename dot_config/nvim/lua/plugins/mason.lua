3f2c747e79eae2260040a6eed171f17a30c22bee
tsantos8080 <tsantos8080@gmail.com>
2025-04-25 10:54:00 -0300

    add keybind to go to references

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -43,6 +43,7 @@ return {
     local on_attach = function()
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
         vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
 	vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 	vim.keymap.set('n', 'r', vim.lsp.buf.rename, {})
     end

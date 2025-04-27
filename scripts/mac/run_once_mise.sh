be850a6cdd9b2d7ee33bf4519edbfd045f017a01
Thyago R. dos Santos <tsantos8080@gmail.com>
2025-04-27 03:19:32 -0300

    fix mise install on mac

--- scripts/mac/run_once_mise.sh
+++ scripts/mac/run_once_mise.sh
@@ -4,11 +4,15 @@ if which mise > /dev/null 2>&1; then
 	echo "\e[32mmise is already installed. skipping...\e[0m"
 else
 	brew install mise
+
+	# PHP dependencies
+	brew install autoconf gmp libsodium bison re2c libxml2 pkgconf libgd libiconv libzip
 fi
 
 # Configure
 mise trust ${PWD}
-mise install
+
+PHP_CONFIGURE_OPTIONS="--with-iconv=$(brew --prefix libiconv) --with-openssl=$(brew --prefix openssl)" \
+	mise install
 
 echo -e "\033[33mPlease run 'source ~/.zshrc' manually for the changes to take effect.\033[0m"
-

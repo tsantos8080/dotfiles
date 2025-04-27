ba0b554fa9e6d2652c1eb146b215055a11ec244c
tsantos8080 <tsantos8080@gmail.com>
2025-04-27 07:35:40 -0300

    add you-should-use plugin to zsh
    fix oh-my-zsh installation on mac

--- scripts/linux/run_once_oh-my-zsh.sh
+++ scripts/linux/run_once_oh-my-zsh.sh
@@ -5,6 +5,7 @@ if grep -q "oh-my-zsh.sh" "$HOME/.zshrc"; then
 else
 	rm -rf ~/.oh-my-zsh
 	sudo apt install -y zsh curl
+
 	RUNZSH=no CHSH=no sh -c "$$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
@@ -16,5 +17,11 @@ if [ -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
 	echo "\e[32moh-my-zsh autosuggestions plugin is already installed. skipping...\e[0m"
 else
 	git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+fi
+
+if [ -d "$HOME/.oh-my-zsh/custom/plugins/zsh-you-should-use" ]; then
+	echo "\e[32moh-my-zsh you-should-use plugin is already installed. skipping...\e[0m"
+else
+	git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
 fi
 

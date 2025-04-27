634162cc640406566333184580d2937278b9e322
tsantos8080 <tsantos8080@gmail.com>
2025-04-27 16:35:26 -0300

    add php formatter

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -8,6 +8,7 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         markdown = { 'markdownlint' },
+        php = { 'pretty-php' },
       },
       format_on_save = {
         timeout_ms = 500,
--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -15,15 +15,16 @@ return {
 
     require('mason-lspconfig').setup({
       ensure_installed = {
-	'phpactor',
-	'quick_lint_js',
+        'phpactor',
+        'quick_lint_js',
       },
     })
 
     require('mason-tool-installer').setup({
       ensure_installed = {
-	'stylua',
-	'markdownlint',
+        'stylua',
+        'markdownlint',
+        'pretty-php',
       },
     })
 
@@ -31,7 +32,7 @@ return {
     cmp.setup({
       snippet = {
         expand = function(args)
-          vim.fn["UltiSnips#Anon"](args.body)
+          vim.fn['UltiSnips#Anon'](args.body)
         end,
       },
       mapping = cmp.mapping.preset.insert({
@@ -40,48 +41,50 @@ return {
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
-	['<tab>'] = cmp.mapping.select_next_item(),
-	['<s-tab>'] = cmp.mapping.select_prev_item(),
+        ['<tab>'] = cmp.mapping.select_next_item(),
+        ['<s-tab>'] = cmp.mapping.select_prev_item(),
       }),
-      sources = cmp.config.sources({{ name = 'nvim_lsp' }}, {{ name = 'buffer' }}, {{ name = 'ultisnips' }}),
+      sources = cmp.config.sources({ { name = 'nvim_lsp' } }, { { name = 'buffer' } }, { { name = 'ultisnips' } }),
     })
 
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
-    
+
     local on_attach = function()
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
 
-	vim.keymap.set('n', 'gr', function()
-	    local clients = vim.lsp.get_active_clients()
-	    local current_buf = vim.api.nvim_get_current_buf()
+      vim.keymap.set('n', 'gr', function()
+        local clients = vim.lsp.get_active_clients()
+        local current_buf = vim.api.nvim_get_current_buf()
 
-	    if vim.tbl_isempty(clients) then
-		print("LSP is not active.")
-		return
-	    end
+        if vim.tbl_isempty(clients) then
+          print('LSP is not active.')
+          return
+        end
 
-	    local references = vim.lsp.buf.definition({
-		on_list = function(opts)
-		    local current_line = vim.api.nvim_win_get_cursor(0)[1]
-		    local current_filename = vim.api.nvim_buf_get_name(0)
-		    local item = opts.items[1]
+        local references = vim.lsp.buf.definition({
+          on_list = function(opts)
+            local current_line = vim.api.nvim_win_get_cursor(0)[1]
+            local current_filename = vim.api.nvim_buf_get_name(0)
+            local item = opts.items[1]
 
-		    if current_line == item.lnum and current_filename == item.filename then
-			vim.lsp.buf.references()
-			return
-		    end
+            if current_line == item.lnum and current_filename == item.filename then
+              vim.lsp.buf.references()
+              return
+            end
 
-		    vim.lsp.buf.definition()
-		end
-	    })
-	end, {})
+            vim.lsp.buf.definition()
+          end,
+        })
+      end, {})
 
-	vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-	vim.keymap.set('n', 'r', vim.lsp.buf.rename, {})
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'r', vim.lsp.buf.rename, {})
     end
 
     require('lspconfig').phpactor.setup({
-      root_dir  = function() return vim.loop.cwd(); end,
+      root_dir = function()
+        return vim.loop.cwd()
+      end,
       on_attach = on_attach,
       capabilities = capabilities,
     })
@@ -92,9 +95,9 @@ return {
     })
   end,
   init = function()
-    vim.cmd [[
+    vim.cmd([[
       let g:UltiSnipsSnippetDirectories = ['~/.config/nvim/custom-snippets']
       let g:UltiSnipsJumpForwardTrigger="<tab>"  
-    ]]
+    ]])
   end,
 }

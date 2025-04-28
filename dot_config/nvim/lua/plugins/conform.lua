e9cddb50c96733cd7f25fd268025046cfac84922
tsantos8080 <tsantos8080@gmail.com>
2025-04-28 12:05:04 -0300

    disable format on save and add a keymap to format changes

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -11,18 +11,43 @@ return {
         php = { 'pretty-php' },
         javascript = { 'prettierd' },
       },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_format = 'fallback',
-      },
     })
 
     vim.keymap.set('n', '<leader>lf', function()
+      local lines = vim.fn.system('git diff --unified=0'):gmatch('[^\n\r]+')
+      local ranges = {}
+      for line in lines do
+        if line:find('^@@') then
+          local line_nums = line:match('%+.- ')
+          if line_nums:find(',') then
+            local _, _, first, second = line_nums:find('(%d+),(%d+)')
+            table.insert(ranges, {
+              start = { tonumber(first), 0 },
+              ['end'] = { tonumber(first) + tonumber(second), 0 },
+            })
+          else
+            local first = tonumber(line_nums:match('%d+'))
+            table.insert(ranges, {
+              start = { first, 0 },
+              ['end'] = { first + 1, 0 },
+            })
+          end
+        end
+      end
+      local format = require('conform').format
+      for _, range in pairs(ranges) do
+        format({
+          range = range,
+        })
+      end
+    end, { desc = 'Format changed lines' })
+
+    vim.keymap.set('n', '<leader>lF', function()
       conform.format({ async = true }, function(err)
         if err ~= nil then
           print(err)
         end
       end)
-    end)
+    end, { desc = 'Format file' })
   end,
 }
